4136ff50ffcdeb9abcc41bbbc4fa7b46c4633a4c
tana <tanasit.istg@mail.kmutt.ac.th>
2023-03-20 02:53:13 +0700

    Refactor Navbar component to replace anchor tag with paragraph tag for 203 Store link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
     <nav class="relative flex w-full flex-wrap items-center justify-start bg-[#DFE6F9] py-3 shadow-lg">
         <div class="flex w-full flex-wrap items-center justify-between px-6">
             <div class="flex basis-auto items-center">
-                <a class="pr-2 text-xl font-extrabold text-black" href="#">203 Store</a>
+                <p class="pr-2 text-xl font-extrabold text-black" href="#">203 Store</p>
                 <ul class="list-style-none mr-auto flex flex-row pl-0">
                     <li class="p-2">
                         <a class="p-0 text-black opacity-60 hover:opacity-80 lg:px-2" href="#">Cashier</a>
