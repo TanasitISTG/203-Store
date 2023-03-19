b2fae34d58e396a3e12882a731ad3cfa9e8369c7
tana <tanasit.istg@mail.kmutt.ac.th>
2023-03-20 01:55:07 +0700

    Add history component and base card for displaying transaction records

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import NavBar from './components/NavBar.vue';
 import Header from './components/Header.vue';
 import Cashier from './components/Cashier/Cashier.vue';
+import History from './components/History/History.vue';
 </script>
 
 <template>
@@ -10,7 +11,8 @@ import Cashier from './components/Cashier/Cashier.vue';
         <Header title="Cashier" style="color: teal" />
     </div>
     <div class="w-full h-full flex justify-center">
-        <Cashier />
+        <!-- <Cashier /> -->
+        <History />
     </div>
 </template>
 
---
+++ src/components/History/History.vue
@@ -0,0 +1,26 @@
+<script setup>
+import BaseCard from '../Base/BaseCard.vue';
+</script>
+
+<template>
+    <BaseCard width="w-3/5" height="h-2/3" backgroundColor="bg-[#F1F1F1]">
+        <div class="flex flex-col mt-5">
+            <div class="flex flex-row">
+                <div class="w-1/4 flex justify-center">
+                    <p class="text-bold text-2xl">Date</p>
+                </div>
+                <div class="w-1/4 flex justify-center">
+                    <p class="text-bold text-2xl">Customer Type</p>
+                </div>
+                <div class="w-1/4 flex justify-center">
+                    <p class="text-bold text-2xl">Discount</p>
+                </div>
+                <div class="w-1/4 flex justify-center">
+                    <p class="text-bold text-2xl">Amount</p>
+                </div>
+            </div>
+        </div>
+    </BaseCard>
+</template>
+
+<style scoped></style>
