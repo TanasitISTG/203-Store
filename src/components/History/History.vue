31ad5053bec82298ac747827e82f2674ad3c1dec
tana <tanasit.istg@mail.kmutt.ac.th>
2023-03-20 03:52:43 +0700

    Add computed property to calculate total amount in History.vue component

--- src/components/History/History.vue
+++ src/components/History/History.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 
 import BaseCard from '../Base/BaseCard.vue';
-import { getAllHistory, getHistoryById } from '../../composables/history';
+import EditIcon from '../Icons/EditIcon.vue';
+import { getAllHistory, getHistoryById } from '../../composables/getHistory';
 
 let historyItems = ref([]);
 
@@ -22,52 +23,68 @@ const deleteHistory = async (id) => {
     }
 };
 
+const getTotalAmount = computed(() => {
+    let total = 0
+    historyItems.value.forEach((item) => {
+        total += parseInt(item.amount);
+    });
+
+    return total;
+});
+
 onMounted(async () => {
     historyItems.value = await getAllHistory();
     console.log(historyItems.value);
 });
 </script>
 
+<!-- could've improve with slot, will do it later. -->
 <template>
     <BaseCard width="w-3/5" height="h-2/3" backgroundColor="bg-[#F1F1F1]">
         <div class="flex flex-col mt-5">
             <div class="flex flex-row">
-                <div>
-
-                </div>
-                <div class="w-1/6 flex justify-center">
+                <div class="w-1/4 flex justify-center">
                     <p class="text-bold text-2xl">Date</p>
                 </div>
-                <div class="w-1/6 flex justify-center">
+                <div class="w-1/4 flex justify-center">
                     <p class="text-bold text-2xl">Customer Type</p>
                 </div>
-                <div class="w-1/6 flex justify-center">
+                <div class="w-1/4 flex justify-center">
                     <p class="text-bold text-2xl">Discount</p>
                 </div>
-                <div class="w-1/6 flex justify-center">
+                <div class="w-1/4 flex justify-center">
                     <p class="text-bold text-2xl">Amount</p>
                 </div>
             </div>
         </div>
 
-        <!-- now loop all data from history -->
+
         <div v-for="item in historyItems" :key="item.id">
-            <!-- <p>{{ item.id }} {{ item.amount }} {{ item.customerType }} {{ item.discount }}</p> -->
             <div class="flex flex-col mt-5">
                 <div class="flex flex-row">
-                    <div class="w-1/6 flex justify-center">
+                    <div class="w-1/4 flex justify-center items-center">
+                        <EditIcon class="mr-2" />
                         <p>{{ item.date }} - {{ item.time }}</p>
                     </div>
-                    <div class="w-1/6 flex justify-center">
+                    <div class="w-1/4 flex justify-center">
                         <p>{{ item.customerType }}</p>
                     </div>
-                    <div class="w-1/6 flex justify-center">
-                        <p>{{ item.discount }}</p>
+                    <div class="w-1/4 flex justify-center">
+                        <p class="text-red-500">{{ item.discount }}</p>
                     </div>
-                    <div class="w-1/6 flex justify-center">
-                        <p>{{ item.amount }}</p>
+                    <div class="w-1/4 flex justify-center">
+                        <p>{{ item.amount }} ฿</p>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="flex flex-row mt-5">
+            <div class="w-1/4 flex justify-center"></div>
+            <div class="w-1/4 flex justify-center"></div>
+            <div class="w-1/4 flex justify-center"></div>
+            <div class="w-1/4 flex justify-center">
+                <p class="text-bold text-2xl">Total {{ getTotalAmount }} ฿</p>
             </div>
         </div>
     </BaseCard>
