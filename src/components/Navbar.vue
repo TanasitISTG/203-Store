dd14b83104822947e2dc6bb45116a44f45bb4fb8
tana <tanasit.istg@mail.kmutt.ac.th>
2023-03-20 18:03:43 +0700

    Refactor Navbar component to improve mobile responsiveness and readability

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,21 +3,17 @@
 </script>
 
 <template>
-    <nav class="relative flex w-full flex-wrap items-center justify-start bg-[#DFE6F9] py-3 shadow-lg">
-        <div class="flex w-full flex-wrap items-center justify-between px-6">
-            <div class="flex basis-auto items-center">
-                <p class="pr-2 text-xl font-extrabold text-black" href="#">203 Store</p>
-                <ul class="list-style-none mr-auto flex flex-row pl-0">
-                    <li class="p-2">
-                        <a class="p-0 text-black opacity-60 hover:opacity-80 lg:px-2" href="#">Cashier</a>
-                    </li>
-                    <li class="p-2">
-                        <a class="p-0 text-black opacity-60 hover:opacity-80 lg:px-2" href="#">History</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="navbar bg-[#DFE6F9] p-0 h-16 min-h-0">
+        <div class="w-1/12 h-full bg-[#304477] max-sm:w-3/12">
+            <p class="text-3xl text-[#FFC635] font-bold mx-auto max-sm:text-lg">203 Store</p>
         </div>
-    </nav>
+        <div class="w-1/12 h-full max-sm:w-3/12">
+            <p class="text-xl font-mono mx-auto max-sm:text-lg">Cashier</p>
+        </div>
+        <div class="w-1/12 h-full max-sm:w-3/12">
+            <p class="text-xl font-mono mx-auto max-sm:text-lg">History</p>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
