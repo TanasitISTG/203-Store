d4d534c206dc50f8413bb9e575e652e7a5d92dfd
tana <tanasit.istg@mail.kmutt.ac.th>
2023-03-20 18:25:46 +0700

    Create History/Row component with four slots for displaying information

---
+++ src/components/History/Row.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-col mt-5">
+        <div class="flex flex-row">
+            <div class="w-1/4 flex justify-center">
+                <slot name="firstRow"></slot>
+            </div>
+            <div class="w-1/4 flex justify-center">
+                <slot name="secondRow"></slot>
+            </div>
+            <div class="w-1/4 flex justify-center">
+                <slot name="thirdRow"></slot>
+            </div>
+            <div class="w-1/4 flex justify-center">
+                <slot name="fourthRow"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
