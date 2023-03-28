b9655e9a399348776231f2de13c502bbc1091342
tana <tanasit.istg@mail.kmutt.ac.th>
2023-03-28 18:20:36 +0700

    Add calculation composable functions for getting total, discount, and total with discount

---
+++ src/composables/calculation.js
@@ -0,0 +1,19 @@
+const getTotal = (amounts) => {
+  let total = 0;
+  amounts.forEach((amount) => {
+    total += amount;
+  });
+
+  return total;
+};
+
+const getDiscount = (discountValue, amounts) => {
+  console.log(getTotal(amounts) * (discountValue / 100));
+  return getTotal(amounts) * (discountValue / 100);
+};
+
+const getTotalWithDiscount = (discountValue, amounts) => {
+  return getTotal(amounts) - getDiscount(discountValue, amounts);
+};
+
+export { getTotal, getDiscount, getTotalWithDiscount };
