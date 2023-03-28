e38f34b55f0b4614dc8fe45451719facbb075e1d
tana <tanasit.istg@mail.kmutt.ac.th>
2023-03-28 18:20:50 +0700

    Refactor Cashier.vue to import calculation composables and use them in getTotalWithDiscount function

--- src/components/Cashier/Cashier.vue
+++ src/components/Cashier/Cashier.vue
@@ -2,27 +2,14 @@
 import { ref, watch } from 'vue';
 import BaseCard from '../Base/BaseCard.vue';
 import DeleteIcon from '../Icons/DeleteIcon.vue';
+import Header from '../Header.vue';
+
+import { getTotal, getDiscount, getTotalWithDiscount } from '../../composables/calculation';
 
 let amount = ref('');
-let amountArray = ref([100, 200, 1, 1, 1]);
+let amountArray = ref([]);
 let discount = ref(0);
 let customerType = ref('Guest')
-
-const getTotal = () => {
-    let total = 0;
-    amountArray.value.forEach((amount) => {
-        total += parseInt(amount);
-    });
-    return total;
-};
-
-const getTotalWithDiscount = () => {
-    return getTotal() - (getTotal() * discount.value / 100);
-}
-
-const getDiscount = () => {
-    return getTotal() * discount.value / 100;
-}
 
 const addAmount = () => {
     if (amount.value === '' || parseInt(amount.value) === 0 || amount.value.length === 0 || isNaN(amount.value) || amount.value === null || amount.value === undefined) {
@@ -46,11 +33,11 @@ const addToHistory = async () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                amount: getTotalWithDiscount(),
+                amount: getTotalWithDiscount(discount.value, amountArray.value),
                 customerType: customerType.value,
-                discount: `${discount.value}%`,
-                date: new Date().toISOString().split('T')[0],
-                time: new Date().toLocaleTimeString('en-US', { hour12: false }),
+                discountPercentage: discount.value,
+                timestamp: new Date().toLocaleString('th-TH', { hour12: false }),
+                amounts: amountArray.value,
                 isEditted: false,
                 oldData: null
             })
@@ -72,19 +59,19 @@ watch([amountArray.value, customerType], () => {
     if (customerType.value === 'Guest') {
         discount.value = 0;
     } else {
-        if (getTotal() >= 400) {
+        if (getTotal(amountArray.value) >= 400) {
             discount.value = 5;
         }
-        if (getTotal() >= 800) {
+        if (getTotal(amountArray.value) >= 800) {
             discount.value = 10;
         }
-        if (getTotal() >= 1200) {
+        if (getTotal(amountArray.value) >= 1200) {
             discount.value = 20;
         }
-        if (getTotal() >= 1600) {
+        if (getTotal(amountArray.value) >= 1600) {
             discount.value = 30;
         }
-        if (getTotal() < 400) {
+        if (getTotal(amountArray.value) < 400) {
             discount.value = 0;
         }
     }
@@ -92,82 +79,91 @@ watch([amountArray.value, customerType], () => {
 </script>
 
 <template>
-    <BaseCard width="w-3/5" height="h-2/3" backgroundColor="bg-[#F1F1F1]">
-        <div class="flex flex-row space-x-4 mt-6 mx-10">
-            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-5xl" v-model="amount"
-                @keyup.enter="addAmount()" />
-            <button class="btn" @click="addAmount()">Add</button>
-        </div>
+    <div class="w-full h-full flex flex-col items-center">
+        <Header title="Cashier" class="m-5" />
+        <BaseCard width="w-3/5" height="h-2/3" backgroundColor="bg-[#F1F1F1]" class="pb-16">
+            <div class="flex flex-row space-x-4 pt-6 mx-10">
+                <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-5xl" v-model="amount"
+                    @keyup.enter="addAmount()" />
+                <button class="btn" @click="addAmount()">Add</button>
+            </div>
 
-        <BaseCard width="w-1/1" height="h-1/3" backgroundColor="bg-white" class="mx-10 mt-6 overflow-auto scrollbar">
-            <div class="w-2/3 h-ful mx-auto pt-4">
-                <p class="text-lg font-semibold">No.</p>
-                <div v-for="(amount, index) in amountArray" :key="index">
-                    <div class="w-full flex flex-row">
-                        <div class="w-1/2 flex justify-start">
-                            <p class="text-lg font-semibold">{{ index + 1 }}. {{ amount }}</p>
+            <BaseCard width="w-1/1" height="h-1/3" backgroundColor="bg-white" class="mx-10 mt-6 overflow-auto scrollbar">
+                <div class="w-2/3 h-ful mx-auto pt-4" v-if="amountArray.length !== 0">
+                    <p class="text-lg font-semibold">No.</p>
+                    <div v-for="(amount, index) in amountArray" :key="index">
+                        <div class="w-full flex flex-row">
+                            <div class="w-1/2 flex justify-start">
+                                <p class="text-lg font-semibold">{{ index + 1 }}. {{ amount }}</p>
+                            </div>
+                            <div class="w-1/2 flex justify-end items-center">
+                                <DeleteIcon class="cursor-pointer" @click="removeAmount(index)" />
+                            </div>
                         </div>
-                        <div class="w-1/2 flex justify-end items-center">
-                            <DeleteIcon class="cursor-pointer" @click="removeAmount(index)" />
+                        <hr class="border-stone-500" />
+                    </div>
+                </div>
+                <div class="h-full flex justify-center items-center" v-else>
+                    <p class="text-lg font-semibold">Add amount to see the list</p>
+                </div>
+            </BaseCard>
+
+            <div class="w-4/5 flex flex-col mx-10">
+                <div class="flex flex-row space-x-2 mt-4">
+                    <p>Customer:</p>
+                    <input type="radio" name="radio-1" class="radio" value="Guest" v-model="customerType" checked />
+                    <p>Guest</p>
+                    <input type="radio" name="radio-1" class="radio radio-base-100" value="Customer"
+                        v-model="customerType" />
+                    <p>Customer</p>
+                </div>
+
+                <div class="flex flex-col mt-4">
+                    <p>Discount ({{ customerType }}): </p>
+                    <div class="flex flex-row space-x-2 mt-2">
+                        <div class="w-20 h-14 rounded-xl" :class="discount === 5 ? 'bg-green-500' : 'bg-[#D7D7D7]'">
+                            <div class="flex flex-col justify-center items-center h-full">
+                                <p class="text-lg font-semibold">5%</p>
+                            </div>
+                        </div>
+                        <div class="w-20 h-14 rounded-xl" :class="discount === 10 ? 'bg-green-500' : 'bg-[#D7D7D7]'">
+                            <div class="flex flex-col justify-center items-center h-full">
+                                <p class="text-lg font-semibold">10%</p>
+                            </div>
+                        </div>
+                        <div class="w-20 h-14 rounded-xl" :class="discount === 20 ? 'bg-green-500' : 'bg-[#D7D7D7]'">
+                            <div class="flex flex-col justify-center items-center h-full">
+                                <p class="text-lg font-semibold">20%</p>
+                            </div>
+                        </div>
+                        <div class="w-20 h-14 rounded-xl" :class="discount === 30 ? 'bg-green-500' : 'bg-[#D7D7D7]'">
+                            <div class="flex flex-col justify-center items-center h-full">
+                                <p class="text-lg font-semibold">30%</p>
+                            </div>
                         </div>
                     </div>
-                    <hr class="border-stone-500" />
+                </div>
+            </div>
+
+            <div class="flex flex-col mx-10">
+                <p class="text-right text-3xl font-semibold" style="color: grey">Subtotal: {{ getTotal(amountArray) }} ฿</p>
+                <p class="text-right text-3xl font-semibold" style="color: red">Discount: {{
+                    getDiscount(discount, amountArray) }} ฿</p>
+                <p class="text-right text-3xl font-semibold" style="color: green">Total: {{ getTotalWithDiscount(discount,
+                    amountArray) }} ฿</p>
+                <div class="flex flex-row justify-end mt-2">
+                    <button class="btn gap-2 w-36" @click="addToHistory()">
+                        Confirm
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </BaseCard>
-
-        <div class="w-4/5 flex flex-col mx-10">
-            <div class="flex flex-row space-x-2 mt-4">
-                <p>Customer:</p>
-                <input type="radio" name="radio-1" class="radio" value="Guest" v-model="customerType" checked />
-                <p>Guest</p>
-                <input type="radio" name="radio-1" class="radio radio-base-100" value="Customer" v-model="customerType" />
-                <p>Customer</p>
-            </div>
-
-            <div class="flex flex-col mt-4">
-                <p>Discount ({{ customerType }}): </p>
-                <div class="flex flex-row space-x-2 mt-2">
-                    <div class="w-20 h-14 rounded-xl" :class="discount === 5 ? 'bg-green-500' : 'bg-[#D7D7D7]'">
-                        <div class="flex flex-col justify-center items-center h-full">
-                            <p class="text-lg font-semibold">5%</p>
-                        </div>
-                    </div>
-                    <div class="w-20 h-14 rounded-xl" :class="discount === 10 ? 'bg-green-500' : 'bg-[#D7D7D7]'">
-                        <div class="flex flex-col justify-center items-center h-full">
-                            <p class="text-lg font-semibold">10%</p>
-                        </div>
-                    </div>
-                    <div class="w-20 h-14 rounded-xl" :class="discount === 20 ? 'bg-green-500' : 'bg-[#D7D7D7]'">
-                        <div class="flex flex-col justify-center items-center h-full">
-                            <p class="text-lg font-semibold">20%</p>
-                        </div>
-                    </div>
-                    <div class="w-20 h-14 rounded-xl" :class="discount === 30 ? 'bg-green-500' : 'bg-[#D7D7D7]'">
-                        <div class="flex flex-col justify-center items-center h-full">
-                            <p class="text-lg font-semibold">30%</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="flex flex-col mx-10">
-            <p class="text-right text-3xl font-semibold" style="color: grey">Subtotal: {{ getTotal() }} ฿</p>
-            <p class="text-right text-3xl font-semibold" style="color: red">Discount: {{ getDiscount() }} ฿</p>
-            <p class="text-right text-3xl font-semibold" style="color: green">Total: {{ getTotalWithDiscount() }} ฿</p>
-            <div class="flex flex-row justify-end mt-2">
-                <button class="btn gap-2 w-36" @click="addToHistory()">
-                    Confirm
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </BaseCard>
+    </div>
 </template>
 
 
