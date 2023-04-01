6865cbd57ab37face673828a7b482cc370ca7a2e
tana <tanasit.istg@mail.kmutt.ac.th>
2023-04-01 17:46:14 +0700

    Add functionality to set, modify and cancel edit mode in History component

--- src/components/History/History.vue
+++ src/components/History/History.vue
@@ -2,13 +2,18 @@
 import { onMounted, ref, computed } from 'vue';
 
 import BaseCard from '../Base/BaseCard.vue';
+import Cashier from '../Cashier/Cashier.vue';
 import EditIcon from '../Icons/EditIcon.vue';
+import DeleteIcon from '../Icons/DeleteIcon.vue';
 import Row from './Row.vue';
 import Header from '../Header.vue';
 
-import { getAllHistory, getHistoryById } from '../../composables/getHistory';
+import { getAllHistory } from '../../composables/getHistory';
+import { numberFormat } from '../../composables/numberFormat';
 
+const currentComponent = ref('History');
 let historyItems = ref([]);
+let editHistory = ref(undefined);
 
 const deleteHistory = async (id) => {
     try {
@@ -28,17 +33,57 @@ const deleteHistory = async (id) => {
 
 const getTotalAmount = computed(() => {
     return historyItems.value.reduce((total, item) => {
-        return total + item.amount;
+        return total + item.total;
     }, 0);
 });
 
+const setEditMode = (history) => {
+    currentComponent.value = 'Cashier';
+    editHistory = history;
+};
+
+const cancelEditMode = async () => {
+    currentComponent.value = 'History';
+    editHistory = undefined;
+    historyItems.value = await getAllHistory();
+};
+
+const modifyHistory = async (updatedHistory) => {
+    try {
+        const response = await fetch(`http://localhost:3001/history/${updatedHistory.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedHistory)
+        });
+
+        if (response.ok) {
+            currentComponent.value = 'History';
+            editHistory = undefined;
+            historyItems.value = historyItems.value.map((item) => {
+                if (item.id === updatedHistory.id) {
+                    return updatedHistory;
+                } else {
+                    return item;
+                }
+            });
+        } else {
+            throw new Error('Something went wrong, please try again later');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 onMounted(async () => {
     historyItems.value = await getAllHistory();
-    console.log(historyItems.value);
 });
 </script>
 
 <template>
+    <Cashier v-if="currentComponent === 'Cashier'" :history="editHistory" @edit="modifyHistory" @cancel="cancelEditMode" />
+
     <div class="w-full h-full flex flex-col items-center">
         <Header title="History" class="m-5" />
         <BaseCard width="w-3/5" height="h-2/3" backgroundColor="bg-[#F1F1F1]" class="scrollbar overflow-auto">
@@ -62,8 +107,9 @@ onMounted(async () => {
             <div v-for="item in historyItems" :key="item.id">
                 <Row>
                     <template #firstRow>
-                        <EditIcon class="mr-2 my-auto" />
-                        <p>{{ item.timestamp }}</p>
+                        <DeleteIcon class="mr-2 my-auto" @click="deleteHistory(item.id)" />
+                        <EditIcon class="mr-2 my-auto" @click="setEditMode(item)" />
+                        <p>{{ item.datetime }}</p>
                     </template>
                     <template #secondRow>
                         <p>{{ item.customerType }}</p>
@@ -72,7 +118,7 @@ onMounted(async () => {
                         <p class="text-red-500">{{ item.discountPercentage }}%</p>
                     </template>
                     <template #fourthRow>
-                        <p>{{ item.amount }} ฿</p>
+                        <p>{{ numberFormat(item.total) }} ฿</p>
                         <div v-if="item.isEdited" class="text-gray-500 ml-3">(edited)</div>
                     </template>
                 </Row>
@@ -80,7 +126,7 @@ onMounted(async () => {
         </BaseCard>
 
         <div class="w-3/5 flex justify-end mt-4">
-            <p class="text-extrabold text-3xl font-poppins">Total {{ getTotalAmount }} ฿</p>
+            <p class="text-extrabold text-3xl font-poppins">Total {{ numberFormat(getTotalAmount) }} ฿</p>
         </div>
     </div>
 </template>
