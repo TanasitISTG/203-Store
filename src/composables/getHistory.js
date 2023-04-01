548bbac8475f103424f2562341fbf17165392a65
tana <tanasit.istg@mail.kmutt.ac.th>
2023-04-01 22:03:02 +0700

    Refactor getHistory.js by removing the unused function getHistoryById and exporting only getAllHistory

--- src/composables/getHistory.js
+++ src/composables/getHistory.js
@@ -13,18 +13,4 @@ const getAllHistory = async () => {
   }
 };
 
-const getHistoryById = async (id) => {
-  try {
-    const data = await fetch(`http://localhost:3001/history/${id}`);
-    if (data.ok) {
-      const history = await data.json();
-      return history;
-    } else {
-      throw new Error("Something went wrong");
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export { getAllHistory, getHistoryById };
+export { getAllHistory };
